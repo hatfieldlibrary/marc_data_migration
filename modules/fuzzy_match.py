d1ec78920019db673da75dc1b258c7ddc4020861
Michael Spalti <mspalti@gmail.com>
2021-04-09 09:56:58 -0700

    Adding logging to fuzzy match

--- modules/fuzzy_match.py
+++ modules/fuzzy_match.py
@@ -4,14 +4,25 @@ from fuzzywuzzy import fuzz
 class FuzzyMatcher:
 
     @staticmethod
-    def findMatch(value1, value2):
-        if fuzz.ratio(value1, value2) >= 80:
+    def log(value1, value2):
+
+    @staticmethod
+    def findMatch(value1, value2, title_log_writer):
+        match_ratio = fuzz.ratio(value1, value2)
+        if match_ratio < 100:
+            title_log_writer.write('Input title: ' + value1 + ', Output title: ' +
+                                   value2 + ' ratio: ' + str(match_ratio))
+        if match_ratio >= 80:
             return True
         return False
 
     @staticmethod
-    def findMatchWithRatio(value1, value2, ratio):
-        if fuzz.ratio(value1, value2) >= ratio:
+    def findMatchWithRatio(value1, value2, ratio, title_log_writer):
+        match_ratio = fuzz.ratio(value1, value2)
+        if match_ratio < 100:
+            title_log_writer.write('Input title: ' + value1 + ', Output title: ' +
+                                   value2 + ' ratio: ' + str(match_ratio))
+        if match_ratio >= ratio:
             return True
         return False
 
