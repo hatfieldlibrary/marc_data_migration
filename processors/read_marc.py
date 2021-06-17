9fc8e99bb602793b58912b4236780a0a0e93a736
Michael W Spalti <mspalti@gmail.com>
2021-06-17 09:39:10 -0700

    More documentation changes.

--- processors/read_marc.py
+++ processors/read_marc.py
@@ -8,13 +8,16 @@ class MarcReader:
         self.handle = open(file, 'rb')
         return MARCReader(self.handle, permissive=permissive, to_unicode=True, utf8_handling=utf8_handling)
 
+    def get_reader_unicode(self, file, encoding: str = 'iso8859-1'):
+        self.handle = open(file, 'rb')
+        # If MARC-8 handling doesn't work it could be that your source file is using an unexpected
+        # character encoding set. If the file is encoded in anything other that iso8859-1 (extended Latin),
+        # try setting the file_encoding parameter accordingly. This will skip marc8 to utf-8 conversion.
+        # Python 3 will covert to utf-8 by default.
+        # In our initial project, Windows-1252 was the right encoding.
+        # See https://docs.python.org/3/library/codecs.html#codec-base-classes for python 3 documentation.
+        # 'replace' will replace invalid utf-8 encoding with a replacement marker.
+        return MARCReader(self.handle, file_encoding=encoding, to_unicode=True, utf8_handling='replace')
+
     def close(self):
         self.handle.close()
-
-    def get_reader_unicode(self, file, encoding='utf-8'):
-        self.handle = open(file, 'rb')
-        # If pymarc's MARC-8 handling doesn't work, provide the encoding, or your best guess.
-        # In our initial project, specifying Windows-1252 was the best solution.
-        # utf8_handling: see https://docs.python.org/3/library/codecs.html#codec-base-classes for details.
-        # 'replace' will replace invalid encoding with a replacement marker.
-        return MARCReader(self.handle, file_encoding='Windows-1252', to_unicode=True, utf8_handling='replace')
